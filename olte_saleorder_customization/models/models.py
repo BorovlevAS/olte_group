28fb5c7e6b5b5fddf3eaffbb3c7699d655906a3f
anastasiia <evdokimova.arh@gmail.com>
2024-01-19 15:26:38 +0200

    [REF] (T3738) olte_saleorder_customization: pre-commit fix

--- olte_saleorder_customization/models/models.py
+++ olte_saleorder_customization/models/models.py
@@ -1,11 +1,12 @@
 from odoo import api, fields, models
 
+
 class SalesOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    product_image = fields.Image(compute='_compute_product_image')
+    product_image = fields.Image(compute="_compute_product_image")
 
-    @api.depends('product_id.image_128')
+    @api.depends("product_id.image_128")
     def _compute_product_image(self):
         for line in self:
             line.product_image = line.product_id.image_128
