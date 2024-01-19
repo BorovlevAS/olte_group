929b89ffb532829f5632f5ab5e24cb89d713cee8
anastasiia <evdokimova.arh@gmail.com>
2024-01-19 15:07:36 +0200

    [ADD] (T3738) olte_saleorder_customization: photo of product in order.line was added

---
+++ olte_saleorder_customization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "BIKO: <olte_saleorder_customization>",
+    'version': '14.0.1.0.0',
+    'author': 'Yevdokimova A.Y.',
+    'company': 'BIKO Solutions',
+    "depends": ["sale"],
+    "data": [
+    'views/view.xml',
+    ],
+
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
